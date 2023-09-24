1c07669127b056f4638cced5865d7c2e577966f0
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2023-09-24 14:29:11 +0200

    fix: fix spark installation folder path

--- scripts/install_spark_environment.sh
+++ scripts/install_spark_environment.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+set -e
+
 echo "Entrée en mode superutilisateur, saisissez votre mot de passe :"
-sudo apt-get update && \
+sudo apt-get update -y && \
+
+echo "Installation de Jupyter Notebook et du kernel Scala..."
+pip install spylon-kernel notebook && \
+python -m spylon_kernel install --user && \
+echo "Jupyter Notebook installé avec succès !"
 
 echo "Installation de Java..."
 sudo apt-get install -y default-jdk && \
@@ -13,21 +20,13 @@ source ~/.profile
 echo "Scala installé avec succès !"
 
 echo "Installation de Spark..."
-wget https://dlcdn.apache.org/spark/spark-3.5.0/spark-3.5.0-bin-hadoop3.tgz -O ~/spark-3.5.0-bin-hadoop3.tgz && \
-tar -xvzf ~/spark-3.5.0-bin-hadoop3.tgz -C ~/ && \
-rm -rf ~/spark-3.5.0-bin-hadoop3.tgz && \
+sudo wget https://dlcdn.apache.org/spark/spark-3.5.0/spark-3.5.0-bin-hadoop3.tgz -O /opt/spark-3.5.0-bin-hadoop3.tgz && \
+sudo tar -xvzf /opt/spark-3.5.0-bin-hadoop3.tgz -C /opt/ && \
+sudo rm -rf /opt/spark-3.5.0-bin-hadoop3.tgz && \
 
-echo "export SPARK_HOME=~/spark-3.5.0-bin-hadoop3" >> ~/.bashrc && \
-echo "export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin" >> ~/.bashrc && \
+echo "export SPARK_HOME=/opt/spark-3.5.0-bin-hadoop3" >> ~/.bashrc && \
+echo "export PATH=$PATH:/opt/spark-3.5.0-bin-hadoop3/bin:/opt/spark-3.5.0-bin-hadoop3/sbin" >> ~/.bashrc && \
 source ~/.bashrc && \
 echo "Spark installé avec succès !"
-
-echo "Installation d'Almond..."
-cs launch almond -- --install && \
-echo "Almond installé avec succès !"
-
-echo "Installation de Jupyter Notebook..."
-pip install notebook && \
-echo "Jupyter Notebook installé avec succès !"
 
 echo "Installation complète ! Lancez make jupyter pour démarrer".
