d0980237904bb4c5f66a15cc561f1e21df37f720
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2023-10-14 01:41:25 +0200

    Multiples fixes for fresh install : - Install build tools for pyenv python versions - Explicitely ask Poetry to use pyenv python 3.9 - Force scripts to exit at error

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,40 +1,45 @@
-#!/bin/bash -i
+#!/bin/bash
 
 set -e
 
-echo "Entrée en mode superutilisateur, saisissez votre mot de passe :"
-echo "Mise à jour des paquets..."
-sudo apt-get update -y -q && \
+PYENV_ROOT="$HOME/.pyenv"
 
-echo "Installation de pyenv..." && \
+echo "Entrée en mode superutilisateur, saisissez votre mot de passe :"
+sudo apt update -y -q 
+sudo apt install build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev curl \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y -q
+
+echo "Installation de pyenv..."
 if [ -d ~/.pyenv ]; then
     echo "pyenv est déjà installé ! Rien à faire."
     exit 0
 fi
 
-curl https://pyenv.run | bash && \
+curl https://pyenv.run | bash
 echo 'export PYENV_ROOT="$HOME/.pyenv"
 command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"' >> ~/.bashrc && \
-source ~/.bashrc && \
+eval "$(pyenv init -)"' >> ~/.bashrc
+source ~/.bashrc
 
 echo "Installation de Python 3.9... (pas de panique, cela prend un certain temps)"
-$HOME/.pyenv/bin/pyenv install 3.9 && \
-$HOME/.pyenv/bin/pyenv global 3.9 && \
+$PYENV_ROOT/bin/pyenv install 3.9
+$PYENV_ROOT/bin/pyenv global 3.9
 echo "Python 3.9 installé avec succès !"
 
 echo "Installation de Poetry..."
-curl -sSL https://install.python-poetry.org | python3 - && \
-poetry config virtualenvs.in-project true && \
+curl -sSL https://install.python-poetry.org | python3 -
+poetry config virtualenvs.in-project true
 echo "Poetry installé avec succès !"
 
 echo "Installation de Jupyter Notebook et du kernel Scala..."
-poetry install && \
-poetry run python -m spylon_kernel install --user && \
+poetry env use $PYENV_ROOT/shims/python3.9
+poetry install
+poetry run python -m spylon_kernel install --user
 echo "Jupyter Notebook installé avec succès !"
 
 echo "Installation de Java..."
-sudo apt-get install -y -q default-jdk && \
+sudo apt-get install -y -q default-jdk
 echo "Java installé avec succès !"
 
 echo "Installation de Scala..."
@@ -43,11 +48,11 @@ source ~/.profile
 echo "Scala installé avec succès !"
 
 echo "Installation de Spark..."
-sudo wget https://dlcdn.apache.org/spark/spark-3.5.0/spark-3.5.0-bin-hadoop3.tgz -O /opt/spark-3.5.0-bin-hadoop3.tgz && \
-sudo tar -xvzf /opt/spark-3.5.0-bin-hadoop3.tgz -C /opt/ && \
-sudo rm -rf /opt/spark-3.5.0-bin-hadoop3.tgz && \
-echo "export SPARK_HOME=/opt/spark-3.5.0-bin-hadoop3" >> ~/.bashrc && \
-echo "export PATH=$PATH:/opt/spark-3.5.0-bin-hadoop3/bin:/opt/spark-3.5.0-bin-hadoop3/sbin" >> ~/.bashrc && \
+sudo wget https://dlcdn.apache.org/spark/spark-3.5.0/spark-3.5.0-bin-hadoop3.tgz -O /opt/spark-3.5.0-bin-hadoop3.tgz
+sudo tar -xvzf /opt/spark-3.5.0-bin-hadoop3.tgz -C /opt/
+sudo rm -rf /opt/spark-3.5.0-bin-hadoop3.tgz
+echo "export SPARK_HOME=/opt/spark-3.5.0-bin-hadoop3" >> ~/.bashrc
+echo "export PATH=$PATH:/opt/spark-3.5.0-bin-hadoop3/bin:/opt/spark-3.5.0-bin-hadoop3/sbin" >> ~/.bashrc
 echo "Spark installé avec succès !"
 
 echo "Installation complète ! Lancez make jupyter pour démarrer".
--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -5,38 +5,38 @@ set -e
 echo "Entrée en mode superutilisateur, saisissez votre mot de passe :"
 sudo echo "Mode superutilisateur activé !"
 
-echo "Désinstallation de Poetry..."
-curl -sSL https://install.python-poetry.org | python3 - --uninstall && \
-echo "Poetry désinstallé avec succès !"
-
 echo "Désinstallation de pyenv..."
-rm -rf ~/.pyenv && \
-sed -i '/pyenv/d' ~/.bashrc && \
-sed -i '/PYENV_ROOT/d' ~/.bashrc && \
-sed -i '/PATH.*PYENV_ROOT/d' ~/.bashrc && \
-sed -i '/eval.*pyenv/d' ~/.bashrc && \
+rm -rf ~/.pyenv
+sed -i '/pyenv/d' ~/.bashrc
+sed -i '/PYENV_ROOT/d' ~/.bashrc
+sed -i '/PATH.*PYENV_ROOT/d' ~/.bashrc
+sed -i '/eval.*pyenv/d' ~/.bashrc
 echo "pyenv désinstallé avec succès !"
 
+echo "Désinstallation de Poetry..."
+curl -sSL https://install.python-poetry.org | python3 - --uninstall
+echo "Poetry désinstallé avec succès !"
+
 echo "Désinstallation de Java..."
-sudo apt-get purge -y -q default-jdk openjdk* && \
-sudo apt-get autoremove -y -q && \
+sudo apt-get purge -y -q default-jdk openjdk*
+sudo apt-get autoremove -y -q
 echo "Java désinstallé avec succès !"
 
 echo "Désinstallation de Scala..."
-cs uninstall --all && \
-rm -rf ~/cs && \
+cs uninstall --all
+rm -rf ~/cs
 echo "Scala désinstallé avec succès !"
 
 echo "Désinstallation de Spark..."
-sudo rm -rf /opt/spark-3.5.0-bin-hadoop3 && \
-sed -i '/SPARK_HOME/d' ~/.bashrc && \
-sed -i '/PATH.*SPARK_HOME/d' ~/.bashrc && \
-source ~/.bashrc && \
+sudo rm -rf /opt/spark-3.5.0-bin-hadoop3
+sed -i '/SPARK_HOME/d' ~/.bashrc
+sed -i '/PATH.*SPARK_HOME/d' ~/.bashrc
+source ~/.bashrc
 echo "Spark désinstallé avec succès !"
 
 echo "Désinstallation de Jupyter et du kernel Scala..."
-rm -rf ~/.venv && \
-rm -rf ~/.local/share/jupyter/kernels/spylon-kernel && \
+rm -rf ~/.venv
+rm -rf ~/.local/share/jupyter/kernels/spylon-kernel
 echo "Jupyter désinstallé avec succès !"
 
 echo "Désinstallation complète ! Relancez votre terminal pour que les changements soient pris en compte."
