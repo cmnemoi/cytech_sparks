a339862f5a1727a220a8c8f02329f3120d597bc7
Charles-Meldhine Madi Mnemoi <icdk5b4zf@relay.firefox.com>
2023-12-04 21:12:44 +0100

    Fix adding pyenv to PATH

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,6 @@ set -e -o pipefail
 POETRY_VERSION="1.6.1"
 PYENV_ROOT="$HOME/.pyenv"
 PYENV_EXEC="$PYENV_ROOT/bin/pyenv"
-PYENV_VERSION="2.3.33"
 PYTHON_VERSION="3.9.18"
 PYTHON_DIR_PATH="$PYENV_ROOT/versions/$PYTHON_VERSION"
 PYTHON_PATH="$PYTHON_DIR_PATH/bin/python3"
@@ -13,10 +12,6 @@ PYTHON_PATH="$PYTHON_DIR_PATH/bin/python3"
 echo "Installation de pyenv..."
 if [ ! -d "$PYENV_ROOT" ]; then
     curl https://pyenv.run | bash
-    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-    echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-    echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
-    source ~/.bashrc
     echo "pyenv installé avec succès !"
 else
     echo "pyenv est déjà installé ! Rien à faire."
@@ -45,14 +40,6 @@ echo "Installation de Java..."
 sudo apt-get install -y -q default-jdk
 echo "Java installé avec succès !"
 
-echo "Installation de Scala..."
-if [ ! ~/cs > /dev/null 2>&1 ]; then
-    curl -fL https://github.com/coursier/coursier/releases/latest/download/cs-x86_64-pc-linux.gz | gzip -d > ~/cs && chmod +x ~/cs && ~/cs setup
-    echo "Scala installé avec succès !"
-else
-    echo "Scala est déjà installé ! Rien à faire."
-fi
-
 echo "Installation de Spark..."
 if [ ! -d "~/spark" ]; then
     mkdir ~/spark # On crée un dossier pour Spark
@@ -69,4 +56,9 @@ else
     echo "Spark est déjà installé ! Rien à faire."
 fi
 
-echo "Installation complète ! Lancez make jupyter pour démarrer".
+# Add pyenv to PATH
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
+
+echo "Installation complète ! Lancez make jupyter dans un nouveau terminal pour commencer".
