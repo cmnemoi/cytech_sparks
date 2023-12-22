2b233bcff0b6af1c3278ff0bbe114a3d20d81d1b
Charles-Meldhine Madi Mnemoi <charlesmeldhine.madimnemoi@servier.com>
2023-12-22 11:38:48 +0100

    Start transform part

--- src/main/scala/TitanticETL.scala
+++ src/main/scala/TitanticETL.scala
@@ -1,15 +1,24 @@
 package cytech_sparks
 
 import org.apache.spark.SparkContext;
-import org.apache.spark.SparkConf;
 import org.apache.spark.rdd.RDD;
+import org.apache.spark.sql.SparkSession;
 
 object TitanicETL {
-    def extract(spark: SparkContext): RDD[String] = {
+    def main(args: Array[String]): Unit = {
+        val spark = SparkSession.builder().appName("TitanicETL").master("local").getOrCreate()
+        var extractedTitanic = extract(spark)
+        var loadedTitanic = load(extractedTitanic)
+        var transformedTitanic = transform(spark, loadedTitanic)
+
+        spark.stop()
+    }
+
+    def extract(spark: SparkSession): RDD[String] = {
         // load titanic files as RDDs
-        val titanicPart1 = spark.textFile("data/titanic_part_1.txt")
-        val titanicPart2 = spark.textFile("data/titanic_part_2.txt")
-        val titanicPart3 = spark.textFile("data/titanic_part_3.gz")
+        val titanicPart1 = spark.sparkContext.textFile("data/titanic_part_1.txt")
+        val titanicPart2 = spark.sparkContext.textFile("data/titanic_part_2.txt")
+        val titanicPart3 = spark.sparkContext.textFile("data/titanic_part_3.gz")
 
         // combine RDDs into one
         val titanic = titanicPart1.union(titanicPart2).union(titanicPart3)
@@ -23,13 +32,23 @@ object TitanicETL {
 
         titanic
     }
-    
-    def main(args: Array[String]): Unit = {
-        val sparkConfig = new SparkConf().setAppName("TitanicETL").setMaster("local")
-        val spark = new SparkContext(sparkConfig)
-        var extractedTitanic = extract(spark)
-        var loadedTitanic = load(extractedTitanic)
 
-        spark.stop()
+    def transform(spark: SparkSession, titanic: RDD[String]) = {
+        typeVariables(spark, titanic)
+        translateToEnglish(titanic)
+    }
+
+    def typeVariables(spark: SparkSession, titanic: RDD[String]) = {
+        // convert rdd to dataframe
+        import spark.implicits._
+        val titanicDataFrame = titanic.toDF()
+
+        // print schema
+        titanicDataFrame.printSchema()
+
+    }
+
+    def translateToEnglish(titanic: RDD[String]) = {
+        // TODO
     }
 }
